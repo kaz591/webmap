1009ec657b598a8e08d98834bcf1a7071478e743
Santiago Rincon <chago591@hotmail.com>
2022-04-23 22:19:36 -0500

    "mapa ahora incluye colorizacion por poblacion"

--- webmap.py
+++ webmap.py
@@ -17,8 +17,21 @@ def color_producer(elevation):
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles = "Stamen Terrain")
 
-for lt, ln, elev in zip(lat, lon,elev):
-    map.add_child(folium.CircleMarker(location=[lt, ln], popup=str(elev)+" m", fill_color = color_producer(elev), fill_opacity =0.8, color= "grey"))
-#icon=folium.Icon(icon="cloud",color=color_producer(elev))))
+fgv = folium.FeatureGroup(name = "Volcanoes")
 
+for lt, ln, elev in zip(lat, lon,elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=str(elev)+" m",
+fill_color = color_producer(elev), fill_opacity =0.8, color= "grey"))
+
+fgp = folium.FeatureGroup(name = "Population")
+
+fgp.add_child(folium.GeoJson(data = open("world.json","r",encoding="utf-8-sig").read(),
+style_function = lambda x: {"fillColor":"green" if x["properties"]["POP2005"] < 10000000
+else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl()) #adds control panel to the top right corner
 map.save("Map1.html")
+
+
